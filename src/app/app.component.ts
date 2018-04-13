198b9c4ad1a1f8c53ded5e72b2d00914bdd69b96
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-04-12 21:00:47 -0700

    used event & property binding and structural directives / disabled the color option for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observable} from "rxjs/Rx";
+
 
 @Component({
 	selector: "application",
 	templateUrl: "./app.html"
 })
-export class AppComponent {
-	public userInput:String = "";
+export class AppComponent implements OnInit {
+	public userInput: string = "";
+	
+	public waitForMe: Observable<boolean>;
+
+	public previousAnswers: string[];
+
+	public ngOnInit(): void {
+		this.waitForMe = Observable.timer(5000).map(() => true);
+		console.log(this.ngOnInit, this.waitForMe);
+		this.previousAnswers = [];
+	}
+
+	addToPrevious(answer: string): void {
+		this.previousAnswers.push(answer);
+	}
+
 }
